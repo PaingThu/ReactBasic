7aeffa31e1a1479373e736445d80cb6e3150b087
Maw Paing Thu <paingmawthu@gmail.com>
2020-04-08 21:40:31 +0900

    Using Component

--- first/src/App.js
+++ first/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+
+class Item extends React.Component{
+  render(){
+    return <li> Content</li>
+  }
+}
+
 class App extends React.Component {
   render(){
     return (
         <div>
           <h1> Hello React </h1>
           <ul>
-            <li>Item One</li>
-            <li>Item Two</li>
+            <Item/>
+            <Item/>
           </ul>
         </div>
       )
