018bf712aa69e0ece8ccdb8ed4c56535fee9e7c5
Maw Paing Thu <paingmawthu@gmail.com>
2020-04-09 13:13:42 +0900

    Input and filling comments

--- first/src/App.js
+++ first/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+// React Component called "Item"
 class Item extends React.Component{
   render(){
     return (
@@ -11,6 +11,13 @@ class Item extends React.Component{
   }
 }
 
+ /*
+   <pre>React Component called "App"</pre>
+   To be a "React Component" class must have the following three facts
+   1. class have to extend "React.Component"
+   2. class must contain "render()" method
+   3. class must return one "Element"
+ */
 class App extends React.Component {
 
   state = {
@@ -19,14 +26,26 @@ class App extends React.Component {
       {id: 2, name: 'Orange', price: 0.89}
     ]
   }
+  // I've used "state" for the Component's data
+
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  //I've used "ref" technology to manage input value
 
   add = () =>{
     let id = this.state.items.length + 1;
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+
     this.setState({
       items:[
-        ...this.state.items,{id,name:`New Item ${id}`,price:0.01*id}
+        ...this.state.items,{id,name,price}
       ]
     });
+    //I've used
+    //"setState()" to change the values of state.
+    //and "spread operator (...)" to spread the array content  of items.
+    //and "property shorthand" (because : items's property name is same with variable name that I used as value in this scope.)
   }
 
   render(){
@@ -38,16 +57,24 @@ class App extends React.Component {
               return(
                 <Item
                   key={i.id}
+                  //without key in a list console show warning as following
+                  //Warning: Each child in  a list should have a unique "key" prop.
+                  //And React cannot use "Virtual DOM" effect if list has no key so you should set "key" prop.
                   name = {i.name}
                   price = {i.price}
                 />
               )
             })}
           </ul>
+          <input type = "text" ref = {this.nameRef} /><br/>
+          <input type = "text" ref = {this.priceRef} /><br/>
           <button onClick={this.add}>Add</button>
         </div>
       )
   }
+  //render() function returns HTML structure.
+  //This HTML structure is HTML(JSX) structure.
+  //To be a react component, must return only one Element so I've written in one <div></div>
 }
 
 export default App;
