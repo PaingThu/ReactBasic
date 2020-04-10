500996e1611e5353ed7fff0eb23925fcc60bb6ef
Maw Paing Thu <paingmawthu@gmail.com>
2020-04-10 11:05:44 +0900

    props Methods

--- react_data_flow/src/App.js
+++ react_data_flow/src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Chapter (3)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Data Flow
-        </a>
-      </header>
-    </div>
-  );
+//React Component called "AddForm"
+class AddForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+
+  add = () => {
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+
+    this.props.add(name,price);
+    //accepting add() of App Component
+  }
+
+  render(){
+    return (
+      <div>
+        <input type = "text" ref = {this.nameRef} /> <br/>
+        <input type = "text" ref = {this.priceRef} /> <br/>
+        <button onClick = {this.add}> Add </button>
+      </div>
+    )
+  }
+}
+
+// React Component called "Item"
+class Item extends React.Component{
+  render(){
+    return (
+      <li>
+        {this.props.name},${this.props.price}
+      </li>
+    )
+  }
+}
+
+//React Component called "App"
+class App extends React.Component {
+
+  state = {
+    items: [
+      {id: 1, name: 'Apple',price: 0.99},
+      {id: 2, name: 'Orange', price: 0.89}
+    ]
+  }
+
+  add = (name,price) =>{
+    let id = this.state.items.length + 1;
+
+    this.setState({
+      items:[
+        ...this.state.items,{id,name,price}
+      ]
+    });
+  }
+
+  render(){
+    return (
+        <div>
+          <h1> Hello React </h1>
+          <ul>
+            {this.state.items.map(i => {
+              return(
+                <Item
+                  key={i.id}
+                  name = {i.name}
+                  price = {i.price}
+                />
+              )
+            })}
+          </ul>
+          <AddForm add = {this.add}/>
+          //AddForm Component with add property that has add() of App Component
+        </div>
+      )
+  }
 }
 
 export default App;
